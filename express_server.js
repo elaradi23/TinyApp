ec30e3248dc0971703cd38d3f7c108e2693489d4
elaradi23 <ta.elaradi23@gmail.com>
2018-07-11 20:58:13 +0000

    Built login/logout feature

--- express_server.js
+++ express_server.js
@@ -1,8 +1,11 @@
 //
 var express = require("express");
 var app = express();
-const bodyParser = require("body-parser");
+var bodyParser = require("body-parser");
+var cookieParser = require('cookie-parser');
+
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(cookieParser());
 
 //
 var PORT = 8080;
@@ -15,7 +18,9 @@ var urlDatabase = {
 
 // Page that desplays the urls in our database:
 app.get("/urls/", (req, res) => {
-  let templateVars = {urls: urlDatabase };
+  // console.log('Cookies: ', req.cookies);
+  // console.log('Signed Cookies: ', req.signedCookies);
+  let templateVars = {urls: urlDatabase, username: req.cookies["username"]};
   res.render("urls_index", templateVars);
 });
 
@@ -28,12 +33,15 @@ app.get("/u/:shortURL", (req, res) => {
 
 // Page to take a new url
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  let templateVars = {username: req.cookies["username"]};
+  res.render("urls_new", templateVars);
 });
 
 // sends a POST request to store the URL with a generated key in our database:
 app.post("/urls/new", (req, res) => {
-  urlDatabase[generateRandomString()] = req.body["longURL"];
+  let key = generateRandomString();
+  urlDatabase[key] = req.body["longURL"];
+  // res.cookie(key, req.body["longURL"]);
   res.redirect("/urls");
 });
 
@@ -54,7 +62,18 @@ app.post("/urls/:shortURL/update", (req, res) =>{
 // Sends a POST request to delete a URL
 app.post("/urls/:id/delete", (req, res) => {
   delete urlDatabase[req.params.id];
-  res.redirect("/urls")
+  res.redirect("/urls");
+});
+
+// Sends a POST request to register username
+app.post('/login', (req, res) => {
+  res.cookie('username', req.body['username']);
+  res.redirect("/urls");
+});
+
+app.post('/logout', (req, res) => {
+  res.clearCookie('username');
+  res.redirect("/urls");
 });
 
 app.listen(PORT, () => {

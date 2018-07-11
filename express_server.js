9441e33ed3dad7e19faa0fc08c827741ffbe3290
elaradi23 <ta.elaradi23@gmail.com>
2018-07-11 00:29:38 +0000

    Added functionality to the express app to accept a given url, store it using a randomly generated key, and the URL can be accessed using that key

--- express_server.js
+++ express_server.js
@@ -1,5 +1,11 @@
+
 var express = require("express");
 var app = express();
+
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
 var PORT = 8080;
 
 app.set("view engine", "ejs");
@@ -9,17 +15,45 @@ var urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+
 app.get("/urls/", (req, res) => {
   let templateVars = {urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
+
+
+app.get("/u/:shortURL", (req, res) => {
+  let longURL = urlDatabase[req.params.shortURL];
+  console.log();
+  res.redirect(longURL);
+});
+
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+// sends a POST request to store the url with a generated key in our database:
+app.post("/urls", (req, res) => {
+  urlDatabase[generateRandomString()] = req.body["longURL"];
+  for(var url in urlDatabase){
+    console.log('\n' + url + ": " + urlDatabase[url]);
+  }
+  res.send("Ok");
+});
+
 
 app.get("/urls/:id", (req, res) => {
   let templateVars = { shortURL: req.params.id };
   res.render("urls_show", templateVars);
 });
 
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
+
+function generateRandomString() {
+  return Math.random().toString(36).replace('0.', '').slice(0, 6);
+}
 
